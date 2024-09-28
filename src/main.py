ff1bc279f3556d8465c2204457ac9cf5a38c22bc
akishiguro <akiosantos@gmail.com>
2024-09-28 18:28:54 -0300

    Configurando o aws sam cli para build e deploy

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import sys
+import awsgi
+from flask import Flask
 from loguru import logger
 
 from services.expose_api_service import ApiService
 
+app = Flask(__name__)
+
 logger.remove(0)
 logger.add(sys.stdout)
 logger.info('start aplication')
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
 
 if __name__ == '__main__':
     ApiService().run()
